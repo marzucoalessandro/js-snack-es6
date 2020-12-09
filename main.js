6e05d7f27cc7b2264955084f555a9b162802101b
Alessandro <marzucoalessandro1@gmail.com>
2020-12-09 16:17:11 +0100

    esercizio 3

--- main.js
+++ main.js
@@ -28,24 +28,50 @@
 
 //l'utente inserisce due parole in successione, con due prompt. il software stampa prima la parola più corta poi la più lunga.
 
-  let parola1 = prompt("scrivi una parola");
-  // console.log(parola1);
-
-  let parola2 = prompt("scrivi una parola");
-  // console.log(parola2);
-
-  const parolaLunga = (word1, word2) => {
-    if (word1.length < word2.length) {
-        return word1 + " " + word2;
-    } else if (word2.length < word1.length) {
-        return word2 +" " + word1;
-    } else {
-      return "parole di egual lunghezza";
-    }
-  };
-
-  console.log(parolaLunga(parola1, parola2));
+  // let parola1 = prompt("scrivi una parola");
+  // // console.log(parola1);
+  //
+  // let parola2 = prompt("scrivi una parola");
+  // // console.log(parola2);
+  //
+  // const parolaLunga = (word1, word2) => {
+  //   if (word1.length < word2.length) {
+  //       return word1 + " " + word2;
+  //   } else if (word2.length < word1.length) {
+  //       return word2 +" " + word1;
+  //   } else {
+  //     return "parole di egual lunghezza";
+  //   }
+  // };
+  //
+  // console.log(parolaLunga(parola1, parola2));
 
   //fine esercizio 2*************************
 
-  
+  // jsnack 3***********************
+  //Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
+
+  // const sommaNumeri = () => {
+  //   let somma = 0;
+  //   for (var i = 1; i < 6; i++) {
+  //
+  //     let number = Number(prompt("dimmi un numero"));
+  //     somma = number + somma;
+  //   } return somma;
+  // }
+  //
+  // console.log(sommaNumeri());
+
+  // const sommaNumeri = () => {
+  //   let somma = 0;
+  //   let i = 1;
+  //
+  //   while (i < 6) {
+  //     let number = Number(prompt("dimmi un numero"));
+  //     somma += number;
+  //     i++
+  //   } return somma;
+  // };
+  // console.log(sommaNumeri());
+
+  // fine esercizio 3*****************//////////
